ceda85a5505501adb6e94994b461ad10bbf915d1
Manuel Hernandez <mahmaruro@outlook.com>
2022-11-29 15:31:07 -0600

    modificar-estructura_correccion-scriptsql

--- src/main/java/mx/uv/sql/script.sql
+++ src/main/java/mx/uv/sql/script.sql
@@ -3,25 +3,28 @@ create database dsuenio;
 use dsuenio;
 
 create table usuario(
+    idUsuario int not null auto_increment,
     usuario varchar(30) not null, 
     contrasenia varchar(30) not null,
     email varchar(50) not null,
     primary key (usuario)
 );
 
-create table sensaciondescanso {
-    idSD int not null AUTO_INCREMENT,
-    nombre varchar not null,
+create table sensaciondescanso (
+    idSD int not null auto_increment,
+    nombre varchar(13) not null,
     primary key (idSD)
-};
+);
 
-create table entrada {
-    idEntrada int not null AUTO_INCREMENT
+create table entrada (
+    idEntrada int not null auto_increment,
+    usuario int not null,
     fechaCreacion varchar(10) not null,
     horasDormidas int not null,
     sensacionDescanso int not null,
     descripcion text(1000) not null,
-    aDestacar varchar(120)
+    aDestacar varchar(120),
     primary key (idEntrada),
-    foreign key (sensacionDescanso) references sensaciondescanso(idSD)
-}
+    foreign key (sensacionDescanso) references sensaciondescanso(idSD),
+    foreign key (usuario) references usuario(usuario)
+);
